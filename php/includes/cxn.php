4da68896c8fc32b02998ecb7df61fba4e926939d
oeddins <oeddins@lastmilehealth.org>
2015-11-11 11:48:35 +0000

    localhost changed to 127.0.0.1
    localhost suddenly stopped working.  Using 127.0.0..1 as a workaround
    seems to work.   Could not find the reason for this behavior in the
    setup files.

--- php/includes/cxn.php
+++ php/includes/cxn.php
@@ -2,12 +2,13 @@
 
 // !!!!! Test app throughout to ensure that DB access errors are handled gracefully !!!!!
 
+//Inexplicably, localhost stopped working, so we now have to use 127.0.0.1.
 // Set connection variable
-$cxn = mysqli_connect("localhost","lastmile_admin","LastMile14") or die("Error");
+$cxn = mysqli_connect("127.0.0.1","lastmile_admin","LastMile14") or die("Error");
 
 // !!!!! When database is down, user gets a non-friendly error message (when logging in, and elsewhere) !!!!!
 
 function getCXN() {
-    $cxn = mysqli_connect("localhost","lastmile_admin","LastMile14") or die("Error");
+    $cxn = mysqli_connect("127.0.0.1","lastmile_admin","LastMile14") or die("Error");
     return $cxn;
 }
