e492c11f477f97470f5e504675d0e2e2745f25e0
Avi Kenny <avi@lastmilehealth.org>
2015-12-10 10:13:20 +0000

    Dump routines for  all databases

--- php/scripts/dataBackup.php
+++ php/scripts/dataBackup.php
@@ -1,9 +1,7 @@
 <?php
 
 // Set DB connection strings (!!!!! figure out how to set include path with CRON !!!!!)
-$user = "lastmile_admin";
-$user1 = "root";
-
+$user = "root";
 $password = "LastMile14";
 $host = "localhost";
 
@@ -12,17 +10,17 @@ $db1 = "lastmile_chwdb";
 $db2 = "lastmile_dataportal";
 $db3 = "lastmile_lms";
 
-
 $backup_file = $db . '_' . date("Y-m-d-H-i-s") . '.sql';
 $backup_file1 = $db1 . '_' . date("Y-m-d-H-i-s") . '.sql';
 $backup_file2 = $db2 . '_' . date("Y-m-d-H-i-s") . '.sql';
 $backup_file3 = $db3 . '_' . date("Y-m-d-H-i-s") . '.sql';
 
-// For Localhost (for testing)
+// For localhost
+// !!!!! Don't delete; reminder of issue with using full mysqldump path !!!!!
 //exec('C:/Users/Avi/Desktop/Avi/xampp/mysql/bin/mysqldump --user="' . $user . '" --password="' . $password . '" --host="' . $host . '" ' . $db . ' >' . $_SERVER['DOCUMENT_ROOT'] . '/LastMileData/backups/' . $backup_file);
 
 // For GoDaddy
-exec('mysqldump --user="' . $user . '" --password="' . $password . '" --host="' . $host . '" ' . $db . ' >' . '/home/lastmilehealth/public_html/LastMileData/backups/' . $backup_file);
-exec('mysqldump --routines --user="' . $user1 . '" --password="' . $password . '" --host="' . $host . '" ' . $db1 . ' >' . '/home/lastmilehealth/public_html/LastMileData/backups/' . $backup_file1);
-exec('mysqldump --user="' . $user . '" --password="' . $password . '" --host="' . $host . '" ' . $db2 . ' >' . '/home/lastmilehealth/public_html/LastMileData/backups/' . $backup_file2);
-exec('mysqldump --user="' . $user . '" --password="' . $password . '" --host="' . $host . '" ' . $db3 . ' >' . '/home/lastmilehealth/public_html/LastMileData/backups/' . $backup_file3);
+exec('mysqldump --routines --user="' . $user . '" --password="' . $password . '" --host="' . $host . '" ' . $db . ' >' . '/home/lastmilehealth/public_html/LastMileData/backups/' . $backup_file);
+exec('mysqldump --routines --user="' . $user . '" --password="' . $password . '" --host="' . $host . '" ' . $db1 . ' >' . '/home/lastmilehealth/public_html/LastMileData/backups/' . $backup_file1);
+exec('mysqldump --routines --user="' . $user . '" --password="' . $password . '" --host="' . $host . '" ' . $db2 . ' >' . '/home/lastmilehealth/public_html/LastMileData/backups/' . $backup_file2);
+exec('mysqldump --routines --user="' . $user . '" --password="' . $password . '" --host="' . $host . '" ' . $db3 . ' >' . '/home/lastmilehealth/public_html/LastMileData/backups/' . $backup_file3);
