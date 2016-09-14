7f9276e2c5c7c154594ddd50ed2fbde8eead4598
Avi Kenny <akenny@lastmilehealth.org>
2016-09-14 10:13:07 +0000

    Added script for convenience to populate DP values

---
+++ php/scripts/call_dataPortalValues.php
@@ -0,0 +1,22 @@
+<?php
+
+// URL
+// localhost/LastMileData/php/scripts/call_dataPortalValues.php?month=X&year=X
+
+// Set include path
+set_include_path( get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] . "/LastMileData/php/includes" );
+
+// Increase maximum execution time
+set_time_limit(600);
+
+// Get parameters
+$month = $_GET['month'];
+$year = $_GET['year'];
+
+// Run query
+require_once("cxn.php");
+$query = "CALL `lastmile_dataportal`.`dataPortalValues`($month, $year);";
+$result = mysqli_query($cxn, $query) or die("failure");
+
+// Display result
+echo $result;
