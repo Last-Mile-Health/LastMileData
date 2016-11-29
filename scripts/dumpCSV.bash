b6a97cad953fd5a7b94cfeb16ac28a8fd4d119a4
oeddins <oeddins@lastmilehealth.org>
2016-11-29 13:05:56 +0000

    Added dump sick child with chw/chwl IDs

--- scripts/dumpCSV.bash
+++ scripts/dumpCSV.bash
@@ -6,21 +6,19 @@ password=LastMile14
 csvFileDirectory=/home/lastmilehealth/public_html/LastMileData/backups/CSVs
 
 # sed commands to convert SQL statement output to csv format.
-
 replaceTab=\ sed\ 's/\t/\",\"/g'\ 
-
 replaceBeginOfLine=sed\ 's/^/\"/g' 
-
 replaceEndOfLine=sed\ 's/$/\"/g'
-
 replaceNULL=sed\ 's/\"NULL\"/\"\"/g'
-
 
 dateStamp=`date +"%Y-%m-%d"`
 
-query='select * from lastmile_chwdb.view_msr'
-fileName=view_msr
+query1='select * from lastmile_chwdb.view_msr'
+fileName1=view_msr
 
-mysql --batch --user=$user --password=$password -e "${query}" | $replaceTab | $replaceBeginOfLine | $replaceEndOfLine | $replaceNULL >  ${csvFileDirectory}/${fileName}_$dateStamp.csv 
+query2='select * from lastmile_chwdb.view_odk_scf_nick'
+fileName2=view_odk_scf_nick
 
-#mysql --user=$user --password=$password -e "${query}" | $replaceTab | $replaceBeginOfLine | $replaceEndOfLine | $replaceNULL >  ${csvFileDirectory}/${fileName}_$dateStamp.csv 
+mysql --batch --user=$user --password=$password -e "${query1}" | $replaceTab | $replaceBeginOfLine | $replaceEndOfLine | $replaceNULL >  ${csvFileDirectory}/${fileName1}_$dateStamp.csv 
+
+mysql --batch --user=$user --password=$password -e "${query2}" | $replaceTab | $replaceBeginOfLine | $replaceEndOfLine | $replaceNULL >  ${csvFileDirectory}/${fileName2}_$dateStamp.csv 
