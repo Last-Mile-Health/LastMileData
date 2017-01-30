0203beb3186bb86b5d46289365434f337049cdf5
oeddins <oeddins@lastmilehealth.org>
2017-01-30 15:33:06 -0700

    add temp chss training to csv dump

--- php/scripts/dumpTablesAsCSVs.php
+++ php/scripts/dumpTablesAsCSVs.php
@@ -25,7 +25,8 @@ executeStatements('lastmile_chwdb','staging_odk_sickChildForm', $cxn);
 executeStatements('lastmile_chwdb','staging_odk_routinevisit', $cxn);
 executeStatements('lastmile_chwdb','staging_odk_vaccinetracker', $cxn);
 executeStatements('lastmile_chwdb','staging_odk_supervisionvisitlog', $cxn);
-executeStatements('lastmile_chwdb','view_msr', $cxn);
+executeStatements('lastmile_upload','temp_chss_training_january2017', $cxn);
+executeStatements('lastmile_upload','temp_chss_training_january2017', $cxn);
 
 function executeStatements($schema, $table, $cxn) {
 
