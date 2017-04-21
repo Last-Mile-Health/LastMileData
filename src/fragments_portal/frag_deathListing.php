bdbeec16164de00e305f4169d291efe57dc7836f
Avi Kenny <avi@lastmilehealth.org>
2017-04-21 13:59:12 -0400

    Report fix

--- src/fragments_portal/frag_deathListing.php
+++ src/fragments_portal/frag_deathListing.php
@@ -13,8 +13,7 @@ require_once("cxn.php");
         <th>Month</th>
         <th>County</th>
         <th>Health Facility</th>
-        <th>CHWL</th>
-        <th>CHW</th>
+        <th>CHA</th>
         <th>Community</th>
         <th>Deaths</th>
     </tr>
@@ -22,20 +21,33 @@ require_once("cxn.php");
 
         $queryString = "
 
-            SELECT yearReported, monthReported, county, healthFacility, CONCAT(chwl_current,' (',chwlID_current,')') as CHWL, CONCAT(chwName,' (',chwID,')') as CHW, CONCAT(community,' (',communityID,')') as Community ,
+            SELECT yearReported, monthReported, county, healthFacility, CONCAT(chwName,' (',chwID,')') as CHA, CONCAT(community,' (',communityID,')') as Community ,
             TRIM(TRAILING ', ' FROM (
-                    CONCAT(
-                            IF(routineVisitsPopulationStillBirthsTotal,CONCAT('stillbirth: ',routineVisitsPopulationStillBirthsTotal,', '),''),
-                            IF(routineVisitsPopulationNeonatalDeathsTotal,CONCAT('neonatal: ',routineVisitsPopulationNeonatalDeathsTotal,', '),''),
-                            IF(routineVisitsPopulationPostNeonatalDeathsTotal,CONCAT('post-neonatal: ',routineVisitsPopulationPostNeonatalDeathsTotal,', '),''),
-                            IF(routineVisitsPopulationChildDeathsTotal,CONCAT('child: ',routineVisitsPopulationChildDeathsTotal,', '),''),
-                            IF(routineVisitsPopulationMaternalDeathsTotal,CONCAT('maternal: ',routineVisitsPopulationMaternalDeathsTotal,', '),'')
-                    )
+                CONCAT(
+                    IF(routineVisitsPopulationStillBirthsTotal,CONCAT('stillbirth: ',routineVisitsPopulationStillBirthsTotal,', '),''),
+                    IF(routineVisitsPopulationNeonatalDeathsTotal,CONCAT('neonatal: ',routineVisitsPopulationNeonatalDeathsTotal,', '),''),
+                    IF(routineVisitsPopulationPostNeonatalDeathsTotal,CONCAT('post-neonatal: ',routineVisitsPopulationPostNeonatalDeathsTotal,', '),''),
+                    IF(routineVisitsPopulationChildDeathsTotal,CONCAT('child: ',routineVisitsPopulationChildDeathsTotal,', '),''),
+                    IF(routineVisitsPopulationMaternalDeathsTotal,CONCAT('maternal: ',routineVisitsPopulationMaternalDeathsTotal,', '),'')
+                )
             )) as `deaths`
             FROM lastmile_chwdb.view_msr
             WHERE (routineVisitsPopulationStillBirthsTotal OR routineVisitsPopulationNeonatalDeathsTotal OR routineVisitsPopulationPostNeonatalDeathsTotal OR routineVisitsPopulationChildDeathsTotal OR routineVisitsPopulationMaternalDeathsTotal)
             AND yearReported IS NOT NULL AND monthReported IS NOT NULL
-            ORDER BY yearReported DESC, monthReported DESC, county DESC, healthFacility DESC, community DESC;
+            UNION SELECT yearReported, monthReported, county, healthFacility, CONCAT(chaName,' (',chaID,')') as CHA, CONCAT(community,' (',communityID,')') as Community ,
+            TRIM(TRAILING ', ' FROM (
+                CONCAT(
+                    IF(nStillBirths,CONCAT('stillbirth: ',nStillBirths,', '),''),
+                    IF(nDeathsNeonatal,CONCAT('neonatal: ',nDeathsNeonatal,', '),''),
+                    IF(nDeathsPostneonatal,CONCAT('post-neonatal: ',nDeathsPostneonatal,', '),''),
+                    IF(nDeathsChild,CONCAT('child: ',nDeathsChild,', '),''),
+                    IF(nDeathsMaternal,CONCAT('maternal: ',nDeathsMaternal,', '),'')
+                )
+            )) as `deaths`
+            FROM lastmile_dataportal._temp_view_msr
+            WHERE (nStillBirths OR nDeathsNeonatal OR nDeathsPostneonatal OR nDeathsChild OR nDeathsMaternal)
+            AND yearReported IS NOT NULL AND monthReported IS NOT NULL
+            ORDER BY CAST(yearReported AS UNSIGNED) DESC, CAST(monthReported AS UNSIGNED) DESC, county DESC, healthFacility DESC, community DESC;
 
         ";
 
@@ -48,8 +60,7 @@ require_once("cxn.php");
             $tableRow .= "<td>$monthReported</td>";
             $tableRow .= "<td>$county</td>";
             $tableRow .= "<td>$healthFacility</td>";
-            $tableRow .= "<td>$CHWL</td>";
-            $tableRow .= "<td>$CHW</td>";
+            $tableRow .= "<td>$CHA</td>";
             $tableRow .= "<td>$Community</td>";
             $tableRow .= "<td>$deaths</td>";
             $tableRow .= "</tr>";
