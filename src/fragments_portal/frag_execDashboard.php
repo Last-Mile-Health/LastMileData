668bd39b449fb20b13c6a75737230a0e36283025
Avi Kenny <avi@lastmilehealth.org>
2017-05-16 16:16:40 +0000

    Update to exec dashboard

--- src/fragments_portal/frag_execDashboard.php
+++ src/fragments_portal/frag_execDashboard.php
@@ -9,7 +9,7 @@
 
     // Set instance IDs of all indicator instances used in the report (in either data tables or charts)
     $instIDString = "336,337,338,339,340,375,341,342,343,344,377,379,381,383,385,376,378,380,382,384,420,"
-            . "422,423,346,348,350,352,354,71,347,349,351,353,415,414,413,412,387,388,231,233,235,297,296,"
+            . "422,423,346,348,350,352,354,71,347,349,351,353,415,414,413,412,533,388,231,233,235,297,296,"
             . "364,365,366,367,368,369,370,371,372,373,374,389,390,391,392,393,394,395,396,406,407,408,"
             . "355,356,357,358,56,359,360,361,362,57,409,410,411,363,418,70,419,424,425,426,427,428,487,68";
 
@@ -90,7 +90,7 @@
                 definition: 'Average number of supervision visits by a CHWL, per CHA ([Total number of supervision visits] / [number of active CHAs]). This currently represents CHWL visits; once the national program rolls out, this will represent CHSS visits, and we will adjust the definition accordingly.',
                 dataSource: 'Monthly service report',
                 groups: [
-                    { name: '', instIDs: [387,388,427] }
+                    { name: '', instIDs: [533,388,427] }
                 ]
             },
             {
