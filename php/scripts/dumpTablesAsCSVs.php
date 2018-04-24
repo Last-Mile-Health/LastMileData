2dd1e954ed1edfe76556db4d2760364b53166f1b
Owen Eddins <oeddins@lastmilehealth.org>
2018-04-24 17:20:17 +0000

    Added all four federated tables

--- php/scripts/dumpTablesAsCSVs.php
+++ php/scripts/dumpTablesAsCSVs.php
@@ -40,7 +40,9 @@ executeStatements('lastmile_program','train_cha', $cxn);
 executeStatements('lastmile_program','train_chss', $cxn);
 
 executeStatements('lastmile_liberiamohdata','federated_de_integrated_supervision_tool_facility', $cxn);
-
+executeStatements('lastmile_liberiamohdata','federated_de_integrated_supervision_tool_facility_spot_check', $cxn);
+executeStatements('lastmile_liberiamohdata','federated_de_integrated_supervision_tool_community', $cxn);
+executeStatements('lastmile_liberiamohdata','federated_de_integrated_supervision_tool_community_spot_check', $cxn);
 
 function executeStatements($schema, $table, $cxn) {
 
