ed0da30c3c7aa0f3944bffe5d6411f49af2dab76
Owen Eddins <oeddins@lastmilehealth.org>
2021-02-03 17:29:35 -0700

    bug: remove password

--- php/includes/cxn.php
+++ php/includes/cxn.php
@@ -4,11 +4,11 @@
 
 //Inexplicably, localhost stopped working, so we now have to use 127.0.0.1.
 // Set connection variable
-$cxn = mysqli_connect("127.0.0.1","lastmile_admin","LastMile14") or die("Error");
+$cxn = mysqli_connect("127.0.0.1","lastmile_admin","") or die("Error");
 
 // !!!!! When database is down, user gets a non-friendly error message (when logging in, and elsewhere) !!!!!
 
 function getCXN() {
-    $cxn = mysqli_connect("127.0.0.1","lastmile_admin","LastMile14") or die("Error");
+    $cxn = mysqli_connect("127.0.0.1","lastmile_admin","") or die("Error");
     return $cxn;
 }
