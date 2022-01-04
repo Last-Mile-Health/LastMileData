cc9cec9dde95c62128f6f089740c7fe2b343e727
Owen Eddins <oeddins@lastmilehealth.org>
2022-01-04 10:12:45 -0600

    feat: credentials

--- php/includes/cxn.php
+++ php/includes/cxn.php
@@ -1,14 +1,24 @@
 <?php
 
-// !!!!! Test app throughout to ensure that DB access errors are handled gracefully !!!!!
+// Credentials file must be a single line with username and password separated by a colon (":").
+$f = fopen( $_SERVER['DOCUMENT_ROOT'] . '/../' . '.credentials', 'r');
+$line = fgets( $f );
+fclose($f);
 
-//Inexplicably, localhost stopped working, so we now have to use 127.0.0.1.
-// Set connection variable
-$cxn = mysqli_connect("127.0.0.1","","") or die("Error");
+// Then, strip all blanks out of substrings and store substring before colon in $username and substring after colon in $password.
+list( $username, $password ) = explode( ":", str_replace( ' ', '', $line ) );
 
-// !!!!! When database is down, user gets a non-friendly error message (when logging in, and elsewhere) !!!!!
+$cxn = mysqli_connect( "127.0.0.1",$username, $password ) or die("Error");
 
+// This function is only called from ./php/scripts/LMD_REST.php
 function getCXN() {
-    $cxn = mysqli_connect("127.0.0.1","","") or die("Error");
+
+    $f = fopen( $_SERVER['DOCUMENT_ROOT'] . '/../' . '.credentials', 'r');
+    $line = fgets( $f );
+    fclose($f);
+    list( $username, $password ) = explode( ":", str_replace( ' ', '', $line ) );
+
+    $cxn = mysqli_connect( "127.0.0.1",$username, $password ) or die("Error");
     return $cxn;
 }
+
